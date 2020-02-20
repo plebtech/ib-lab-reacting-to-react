9b5312dafa0cfa24f63098f59be9a1291e7641a8
Nick Mc <nbmc@protonmail.com>
2020-02-20 13:59:32 -0600

    through step 9.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,41 +4,51 @@ const divPadding = {
     margin: '2% auto',
 }
 
-let hasLoaded = false;
-
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             text: 'this is some text from state',
             placeholder: 'placeholder',
+            hasLoaded: false,
         }
         this.greeting = this.props.greeting;
     }
     handleInputChange = (val) => {
         this.setState({ input: val });
     }
-    toggleHasLoaded() {
-        hasLoaded = !hasLoaded;
-        console.log(hasLoaded);
+    // toggleHasLoaded() {
+    //     hasLoaded = !hasLoaded;
+    //     console.log(hasLoaded);
+    toggleHasLoaded = () => {
+        this.setState({ hasLoaded: !this.state.hasLoaded });
     }
     render() {
-        return (
-            <React.Fragment>
-                <h1>
-                    {this.greeting}
-                </h1>
-                <h2>
-                    {this.state.text}
-                </h2>
-                <div><input
-                    placeholder={this.state.placeholder}
-                    value={this.state.input}
-                    onChange={(e) => this.handleInputChange(e.target.value)}
-                ></input></div>
-                <div style={divPadding}><button onClick={this.toggleHasLoaded}>toggle hasLoaded</button></div>
-            </React.Fragment>
-        )
+        if (this.state.hasLoaded) {
+            return (
+                <React.Fragment>
+                    <h1>
+                        {this.greeting}
+                    </h1>
+                    <h2>
+                        {this.state.text}
+                    </h2>
+                    <div><input
+                        placeholder={this.state.placeholder}
+                        value={this.state.input}
+                        onChange={(e) => this.handleInputChange(e.target.value)}
+                    ></input></div>
+                    <div style={divPadding}><button onClick={this.toggleHasLoaded}>toggleHasLoaded</button></div>
+                </React.Fragment>
+            )
+        } else {
+            return (
+                <React.Fragment>
+                    <h1>Loading..</h1>
+                    <div style={divPadding}><button onClick={this.toggleHasLoaded}>toggleHasLoaded</button></div>
+                </React.Fragment>
+            )
+        }
     }
 }
 
